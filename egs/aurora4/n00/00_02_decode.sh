63d5e2abdd0916bc95aec3a3f06cd27b270180f0
Troy Lee <troy.lee2008@gmail.com>
2014-06-26 09:54:57 +0800

    decoding script

--- egs/aurora4/n00/00_02_decode.sh
+++ egs/aurora4/n00/00_02_decode.sh
@@ -7,7 +7,7 @@
 cwd=~/tools/kaldi/egs/aurora4/n00
 cd $cwd
 
-nodes=( compg2 compg4 compg5 compg6 compg7 compg8 compg9 )
+nodes=( compg2 compg5 compg6 compg7 compg8 compg9 compg10 )
 
 set -e           #Exit on non-zero return code from any command
 set -o pipefail  #Exit if any of the commands in the pipeline will 
@@ -83,7 +83,7 @@ decode_clean_tri1b_vtsmodel(){
   local/average_wer.sh 'exp_clean/tri1b/decode_vts_model/decode_bg_test*' | tee exp_clean/tri1b/decode_vts_model/decode_bg_test.avgwer
   log_end "tri1b [vtsmodel decode]"
 }
-decode_clean_tri1b_vtsmodel
+#decode_clean_tri1b_vtsmodel
 
 
 decode_multi_tri1a(){
@@ -123,60 +123,210 @@ decode_multi_tri1b(){
 }
 #decode_multi_tri1b
 
-
-decode_multi_tri2a(){
-  # decode exp_multi/tri2a_dnn
-  log_start "tri2a [decode]"
-  for i in {0..6} ; do 
-    printf -v x 'test%02g' $((i+1))
-    echo ${nodes[$i]} $x
-    ( ssh ${nodes[$i]} "cd $cwd; steps/aurora4/nnet_decode.sh --nj 5 --srcdir exp_multi/tri2a_dnn exp_multi/tri2a_dnn/graph_bg feat/fbank/${x} exp_multi/tri2a_dnn/decode/decode_bg_${x}" ) &
-    
-    printf -v x 'test%02g' $((i+8))
-    echo ${nodes[$i]} $x
-    ( ssh ${nodes[$i]} "cd $cwd; steps/aurora4/nnet_decode.sh --nj 5 --srcdir exp_multi/tri2a_dnn exp_multi/tri2a_dnn/graph_bg feat/fbank/${x} exp_multi/tri2a_dnn/decode/decode_bg_${x}" ) &
-  done
-  wait;
-  # write out the average WER results
-  local/average_wer.sh --min-lmwt 9 --max-lmwt 20 'exp_multi/tri2a_dnn/decode/decode_bg_test*' | tee exp_multi/tri2a_dnn/decode/decode_bg_test.avgwer
-  log_end "tri2a [decode]"
-}
-#decode_multi_tri2a
-
-decode_multi_tri2b(){
-  # decode exp_multi/tri2b_dnn
-  log_start "tri2b [decode]"
+decode_multi_tri1b_vtsmodel(){
+  log_start "tri1b [vtsmodel decode]"
   for i in {0..6} ; do
     printf -v x 'test%02g' $((i+1))
     echo ${nodes[$i]} $x
-    ( ssh ${nodes[$i]} "cd $cwd; steps/aurora4/nnet_decode.sh --nj 5 --srcdir exp_multi/tri2b_dnn exp_multi/tri2b_dnn/graph_bg feat/fbank/${x} exp_multi/tri2b_dnn/decode/decode_bg_${x}" ) &
+    ( ssh ${nodes[$i]} "cd $cwd; steps/aurora4/decode_vts_model.sh --nj 5 --srcdir exp_multi/tri1b exp_multi/tri1b/graph_bg feat/mfcc/${x} exp_multi/tri1b/decode_vts_model/decode_bg_${x}" ) &
 
     printf -v x 'test%02g' $((i+8))
     echo ${nodes[$i]} $x
-    ( ssh ${nodes[$i]} "cd $cwd; steps/aurora4/nnet_decode.sh --nj 5 --srcdir exp_multi/tri2b_dnn exp_multi/tri2b_dnn/graph_bg feat/fbank/${x} exp_multi/tri2b_dnn/decode/decode_bg_${x}" ) &
+    ( ssh ${nodes[$i]} "cd $cwd; steps/aurora4/decode_vts_model.sh --nj 5 --srcdir exp_multi/tri1b exp_multi/tri1b/graph_bg feat/mfcc/${x} exp_multi/tri1b/decode_vts_model/decode_bg_${x}" ) &
   done
   wait;
   # write out the average WER results
-  local/average_wer.sh --min-lmwt 9 --max-lmwt 20 'exp_multi/tri2b_dnn/decode/decode_bg_test*' | tee exp_multi/tri2b_dnn/decode/decode_bg_test.avgwer
-  log_end "tri2b [decode]"
+  local/average_wer.sh 'exp_multi/tri1b/decode_vts_model/decode_bg_test*' | tee exp_multi/tri1b/decode_vts_model/decode_bg_test.avgwer
+  log_end "tri1b [vtsmodel decode]"
 }
-#decode_multi_tri2b
+#decode_multi_tri1b_vtsmodel
 
 decode_multi_tri3a(){
   log_start "tri3a [decode]"
-  for i in {0..6} ; do
+  dir=exp_multi/tri3a_dnn
+  for i in {0..6} ; do 
     printf -v x 'test%02g' $((i+1))
     echo ${nodes[$i]} $x
-    ( ssh ${nodes[$i]} "cd $cwd; steps/aurora4/nnet_decode.sh --nj 5 --srcdir exp_multi/tri3a_dnn exp_multi/tri3a_dnn/graph_bg feat/fbank/${x} exp_multi/tri3a_dnn/decode/decode_bg_${x}" ) &
-
+    ( ssh ${nodes[$i]} "cd $cwd; steps/aurora4/nnet_decode.sh --nj 5 --srcdir $dir $dir/graph_bg feat/fbank/${x} $dir/decode/decode_bg_${x}" ) &
+    
     printf -v x 'test%02g' $((i+8))
     echo ${nodes[$i]} $x
-    ( ssh ${nodes[$i]} "cd $cwd; steps/aurora4/nnet_decode.sh --nj 5 --srcdir exp_multi/tri3a_dnn exp_multi/tri3a_dnn/graph_bg feat/fbank/${x} exp_multi/tri3a_dnn/decode/decode_bg_${x}" ) &
+    ( ssh ${nodes[$i]} "cd $cwd; steps/aurora4/nnet_decode.sh --nj 5 --srcdir $dir $dir/graph_bg feat/fbank/${x} $dir/decode/decode_bg_${x}" ) &
   done
   wait;
   # write out the average WER results
-  local/average_wer.sh --min-lmwt 9 --max-lmwt 20 'exp_multi/tri3a_dnn/decode/decode_bg_test*' | tee exp_multi/tri3a_dnn/decode/decode_bg_test.avgwer
+  local/average_wer.sh --min-lmwt 9 --max-lmwt 20 "$dir/decode/decode_bg_test*" | tee $dir/decode/decode_bg_test.avgwer
   log_end "tri3a [decode]"
 }
 #decode_multi_tri3a
 
+decode_multi_tri4a(){
+  log_start "tri4a [decode]"
+  dir=exp_multi/tri4a_dnn
+  for i in {0..6} ; do
+    printf -v x 'test%02g' $((i+1))
+    echo ${nodes[$i]} $x
+    ( ssh ${nodes[$i]} "cd $cwd; steps/aurora4/nnet_decode.sh --nj 5 --srcdir $dir $dir/graph_bg feat/fbank/${x} $dir/decode/decode_bg_${x}" ) &
+
+    printf -v x 'test%02g' $((i+8))
+    echo ${nodes[$i]} $x
+    ( ssh ${nodes[$i]} "cd $cwd; steps/aurora4/nnet_decode.sh --nj 5 --srcdir $dir $dir/graph_bg feat/fbank/${x} $dir/decode/decode_bg_${x}" ) &
+  done
+  wait;
+  # write out the average WER results
+  local/average_wer.sh --min-lmwt 9 --max-lmwt 20 "$dir/decode/decode_bg_test*" | tee $dir/decode/decode_bg_test.avgwer
+  log_end "tri4a [decode]"
+}
+#decode_multi_tri4a
+
+decode_multi_tri5a(){
+  log_start "tri5a [decode]"
+  dir=exp_multi/tri5a_dnn
+  for i in {0..6} ; do
+    printf -v x 'test%02g' $((i+1))
+    echo ${nodes[$i]} $x
+    ( ssh ${nodes[$i]} "cd $cwd; steps/aurora4/nnet_decode.sh --nj 5 --srcdir $dir $dir/graph_bg feat/fbank/${x} $dir/decode/decode_bg_${x}" ) &
+
+    printf -v x 'test%02g' $((i+8))
+    echo ${nodes[$i]} $x
+    ( ssh ${nodes[$i]} "cd $cwd; steps/aurora4/nnet_decode.sh --nj 5 --srcdir $dir $dir/graph_bg feat/fbank/${x} $dir/decode/decode_bg_${x}" ) &
+  done
+  wait;
+  # write out the average WER results
+  local/average_wer.sh --min-lmwt 9 --max-lmwt 20 "$dir/decode/decode_bg_test*" | tee $dir/decode/decode_bg_test.avgwer
+  log_end "tri5a [decode]"
+}
+decode_multi_tri5a
+
+decode_multi_tri3b(){
+  log_start "tri3b [decode]"
+  dir=exp_multi/tri3b_dnn
+  for i in {0..6} ; do
+    printf -v x 'test%02g' $((i+1))
+    echo ${nodes[$i]} $x
+    ( ssh ${nodes[$i]} "cd $cwd; steps/aurora4/nnet_decode.sh --nj 5 --srcdir $dir $dir/graph_bg feat/fbank/${x} $dir/decode/decode_bg_${x}" ) &
+
+    printf -v x 'test%02g' $((i+8))
+    echo ${nodes[$i]} $x
+    ( ssh ${nodes[$i]} "cd $cwd; steps/aurora4/nnet_decode.sh --nj 5 --srcdir $dir $dir/graph_bg feat/fbank/${x} $dir/decode/decode_bg_${x}" ) &
+  done
+  wait;
+  # write out the average WER results
+  local/average_wer.sh --min-lmwt 9 --max-lmwt 20 "$dir/decode/decode_bg_test*" | tee $dir/decode/decode_bg_test.avgwer
+  log_end "tri3b [decode]"
+}
+#decode_multi_tri3b
+
+decode_multi_tri4b(){
+  log_start "tri4b [decode]"
+  dir=exp_multi/tri4b_dnn
+  for i in {0..6} ; do
+    printf -v x 'test%02g' $((i+1))
+    echo ${nodes[$i]} $x
+    ( ssh ${nodes[$i]} "cd $cwd; steps/aurora4/nnet_decode.sh --nj 5 --srcdir $dir $dir/graph_bg feat/fbank/${x} $dir/decode/decode_bg_${x}" ) &
+
+    printf -v x 'test%02g' $((i+8))
+    echo ${nodes[$i]} $x
+    ( ssh ${nodes[$i]} "cd $cwd; steps/aurora4/nnet_decode.sh --nj 5 --srcdir $dir $dir/graph_bg feat/fbank/${x} $dir/decode/decode_bg_${x}" ) &
+  done
+  wait;
+  # write out the average WER results
+  local/average_wer.sh --min-lmwt 9 --max-lmwt 20 "$dir/decode/decode_bg_test*" | tee $dir/decode/decode_bg_test.avgwer
+  log_end "tri4b [decode]"
+}
+#decode_multi_tri4b
+
+decode_multi_tri5b(){
+  log_start "tri5b [decode]"
+  dir=exp_multi/tri5b_dnn
+  for i in {0..6} ; do
+    printf -v x 'test%02g' $((i+1))
+    echo ${nodes[$i]} $x
+    ( ssh ${nodes[$i]} "cd $cwd; steps/aurora4/nnet_decode.sh --nj 5 --srcdir $dir $dir/graph_bg feat/fbank/${x} $dir/decode/decode_bg_${x}" ) &
+
+    printf -v x 'test%02g' $((i+8))
+    echo ${nodes[$i]} $x
+    ( ssh ${nodes[$i]} "cd $cwd; steps/aurora4/nnet_decode.sh --nj 5 --srcdir $dir $dir/graph_bg feat/fbank/${x} $dir/decode/decode_bg_${x}" ) &
+  done
+  wait;
+  # write out the average WER results
+  local/average_wer.sh --min-lmwt 9 --max-lmwt 20 "$dir/decode/decode_bg_test*" | tee $dir/decode/decode_bg_test.avgwer
+  log_end "tri5b [decode]"
+}
+#decode_multi_tri5b
+
+decode_multi_tri3c(){
+  log_start "tri3c [decode]"
+  dir=exp_multi/tri3c_dnn
+  for i in {0..6} ; do
+    printf -v x 'test%02g' $((i+1))
+    echo ${nodes[$i]} $x
+    ( ssh ${nodes[$i]} "cd $cwd; steps/aurora4/nnet_decode.sh --nj 5 --srcdir $dir $dir/graph_bg feat/fbank/${x} $dir/decode/decode_bg_${x}" ) &
+
+    printf -v x 'test%02g' $((i+8))
+    echo ${nodes[$i]} $x
+    ( ssh ${nodes[$i]} "cd $cwd; steps/aurora4/nnet_decode.sh --nj 5 --srcdir $dir $dir/graph_bg feat/fbank/${x} $dir/decode/decode_bg_${x}" ) &
+  done
+  wait;
+  # write out the average WER results
+  local/average_wer.sh --min-lmwt 9 --max-lmwt 20 "$dir/decode/decode_bg_test*" | tee $dir/decode/decode_bg_test.avgwer
+  log_end "tri3c [decode]"
+}
+#decode_multi_tri3c
+
+decode_multi_tri4c(){
+  log_start "tri4c [decode]"
+  dir=exp_multi/tri4c_dnn
+  for i in {0..6} ; do
+    printf -v x 'test%02g' $((i+1))
+    echo ${nodes[$i]} $x
+    ( ssh ${nodes[$i]} "cd $cwd; steps/aurora4/nnet_decode.sh --nj 5 --srcdir $dir $dir/graph_bg feat/fbank/${x} $dir/decode/decode_bg_${x}" ) &
+
+    printf -v x 'test%02g' $((i+8))
+    echo ${nodes[$i]} $x
+    ( ssh ${nodes[$i]} "cd $cwd; steps/aurora4/nnet_decode.sh --nj 5 --srcdir $dir $dir/graph_bg feat/fbank/${x} $dir/decode/decode_bg_${x}" ) &
+  done
+  wait;
+  # write out the average WER results
+  local/average_wer.sh --min-lmwt 9 --max-lmwt 20 "$dir/decode/decode_bg_test*" | tee $dir/decode/decode_bg_test.avgwer
+  log_end "tri4c [decode]"
+}
+#decode_multi_tri4c
+
+decode_multi_tri3d(){
+  log_start "tri3d [decode]"
+  dir=exp_multi/tri3d_dnn
+  for i in {0..6} ; do
+    printf -v x 'test%02g' $((i+1))
+    echo ${nodes[$i]} $x
+    ( ssh ${nodes[$i]} "cd $cwd; steps/aurora4/nnet_decode.sh --nj 5 --srcdir $dir $dir/graph_bg feat/fbank/${x} $dir/decode/decode_bg_${x}" ) &
+
+    printf -v x 'test%02g' $((i+8))
+    echo ${nodes[$i]} $x
+    ( ssh ${nodes[$i]} "cd $cwd; steps/aurora4/nnet_decode.sh --nj 5 --srcdir $dir $dir/graph_bg feat/fbank/${x} $dir/decode/decode_bg_${x}" ) &
+  done
+  wait;
+  # write out the average WER results
+  local/average_wer.sh --min-lmwt 9 --max-lmwt 20 "$dir/decode/decode_bg_test*" | tee $dir/decode/decode_bg_test.avgwer
+  log_end "tri3d [decode]"
+}
+#decode_multi_tri3d
+
+decode_multi_tri4d(){
+  log_start "tri4d [decode]"
+  dir=exp_multi/tri4d_dnn
+  for i in {0..6} ; do
+    printf -v x 'test%02g' $((i+1))
+    echo ${nodes[$i]} $x
+    ( ssh ${nodes[$i]} "cd $cwd; steps/aurora4/nnet_decode.sh --nj 5 --srcdir $dir $dir/graph_bg feat/fbank/${x} $dir/decode/decode_bg_${x}" ) &
+
+    printf -v x 'test%02g' $((i+8))
+    echo ${nodes[$i]} $x
+    ( ssh ${nodes[$i]} "cd $cwd; steps/aurora4/nnet_decode.sh --nj 5 --srcdir $dir $dir/graph_bg feat/fbank/${x} $dir/decode/decode_bg_${x}" ) &
+  done
+  wait;
+  # write out the average WER results
+  local/average_wer.sh --min-lmwt 9 --max-lmwt 20 "$dir/decode/decode_bg_test*" | tee $dir/decode/decode_bg_test.avgwer
+  log_end "tri4d [decode]"
+}
+#decode_multi_tri4d
